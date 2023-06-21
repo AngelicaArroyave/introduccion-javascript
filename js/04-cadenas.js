5445436bf8549692180e06e4545fee6fe838e0c6
DESKTOP-1TC6Q9U\USUARIO <aarroyaveduque20@gmail.com>
2023-06-20 21:23:12 -0500

    Manejo de strings o cadenas de texto

---
+++ js/04-cadenas.js
@@ -0,0 +1,16 @@
+// String o cadenas de texto
+
+const product = 'Monitor de 20 pulgadas'
+console.log(product);
+
+const product2 = String('Monitor de 30 pulgadas')
+console.log(product2);
+
+const product3 = new String('Monitor de 43 pulgadas') // Es un objeto
+console.log(product3, typeof product3);
+
+const product4 = 'Monitor de 29"'
+console.log(product4);
+
+const product5 = "Monitor de 27\""
+console.log(product5);
