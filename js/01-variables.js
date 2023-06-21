4aaab348575e7b1795bebf6659425e0af2404584
DESKTOP-1TC6Q9U\USUARIO <aarroyaveduque20@gmail.com>
2023-06-20 20:28:18 -0500

    Manejo de variables (var, forma de nombarlas, asignación)

---
+++ js/01-variables.js
@@ -0,0 +1,16 @@
+// Variables
+
+var product = 'Audifonos Gamer' // Iniciar variable y asignarle valor
+var available // Iniciar variable pero sin valor
+
+product = true // Reasignando el valor de la variable
+available = true
+
+var product1 = 'Computadora',
+    available1 = true,
+    category = 'Computadoras'
+
+var name_product = 'Monitor HD' // Underscore
+var nameProduct = 'Monitor HD' // Camelcase
+var NameProduct = 'Monitor HD' // Pascal Case
+var nameproduct = 'Monitor HD' // Lower Case
