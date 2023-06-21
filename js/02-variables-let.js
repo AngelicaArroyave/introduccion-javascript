2f3534adc865f52c20df8ac59f801d4b7af719e7
DESKTOP-1TC6Q9U\USUARIO <aarroyaveduque20@gmail.com>
2023-06-20 20:35:27 -0500

    Manejo de variables let y const

---
+++ js/02-variables-let.js
@@ -0,0 +1,16 @@
+// Variables let y const
+
+let product = 'Audifonos Gamer'
+let available
+
+product = true
+available = true
+
+let product1 = 'Computadora',
+    available1 = true,
+    category = 'Computadoras'
+
+let name_product = 'Monitor HD'
+let nameProduct = 'Monitor HD'
+let NameProduct = 'Monitor HD'
+let nameproduct = 'Monitor HD'
---
+++ js/03-variables-const.js
@@ -0,0 +1,16 @@
+// Variables let y const
+
+const product = 'Audifonos Gamer'
+// const available // No se puede dejar solamente el nombre de la variable, hay que inicializarla
+
+product = true // No se puede reasignar o cambiar el valor
+available = true
+
+const product1 = 'Computadora',
+    available1 = true,
+    category = 'Computadoras'
+
+const name_product = 'Monitor HD'
+const nameProduct = 'Monitor HD'
+const NameProduct = 'Monitor HD'
+const nameproduct = 'Monitor HD'
