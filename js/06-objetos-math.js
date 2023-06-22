af325f12f1f38d74ba07296f3f387b1d0784155a
DESKTOP-1TC6Q9U\USUARIO <aarroyaveduque20@gmail.com>
2023-06-21 19:19:15 -0500

    Manejo de objeto Math

---
+++ js/06-objetos-math.js
@@ -0,0 +1,30 @@
+// Objeto Math
+
+let result
+
+result = Math.PI
+console.log(result); // 3.141592653589793
+
+result = Math.round(2.2)
+console.log(result); // 2
+
+result = Math.ceil(2.9) // Siempre redondea hacia arriba
+console.log(result); // 3
+
+result = Math.floor(2.9) // Siempre redondea hacia abajo
+console.log(result); // 2
+
+result = Math.sqrt(144) // Raìz cuadrada
+console.log(result); // 12
+
+result = Math.abs(-144) // Valor absoluto de un número
+console.log(result); // 144
+
+result = Math.min(2, 4, 6, 8, 9, 16) // Retorna el valor más pequeño de una serie de números
+console.log(result); // 2
+
+result = Math.max(2, 4, 6, 8, 9, 16) // Retorna el valor más grande de una serie de números
+console.log(result); // 16
+
+result = Math.random() // Retorna un número aleatorio
+console.log(result);
