810dbf4acbca31db6bb1cf450396716081b2135c
DESKTOP-1TC6Q9U\USUARIO <aarroyaveduque20@gmail.com>
2023-06-21 19:09:15 -0500

    Manejo de números y sus operaciones

---
+++ js/05-numeros.js
@@ -0,0 +1,18 @@
+// Números
+
+const number1 = 100
+const number2 = 200
+const number3 = 200.20
+const number4 = .1020
+const number5 = -5
+
+console.log(number1);
+console.log(number2);
+console.log(number3);
+console.log(number4);
+console.log(number5);
+console.log(number1 + number2);
+console.log(number2 - number1);
+console.log(number2 * number1);
+console.log(number2 % number1);
+console.log(number2 / number1);
