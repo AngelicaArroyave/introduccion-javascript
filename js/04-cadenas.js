237a64267bbb78a0f6dfc37dc6a9cb113ca5a4a7
DESKTOP-1TC6Q9U\USUARIO <aarroyaveduque20@gmail.com>
2023-06-21 19:00:28 -0500

    Manejo de métodos en cadenas de texto

--- js/04-cadenas.js
+++ js/04-cadenas.js
@@ -1,16 +1,21 @@
 // String o cadenas de texto
 
 const product = 'Monitor de 20 pulgadas'
-console.log(product);
+console.log(product); // Monitor de 20 pulgadas
 
 const product2 = String('Monitor de 30 pulgadas')
-console.log(product2);
+console.log(product2); // Monitor de 30 pulgadas
 
 const product3 = new String('Monitor de 43 pulgadas') // Es un objeto
-console.log(product3, typeof product3);
+console.log(product3, typeof product3); // Monitor de 43 pulgadas
 
 const product4 = 'Monitor de 29"'
-console.log(product4);
+console.log(product4); // Monitor de 29"
 
 const product5 = "Monitor de 27\""
-console.log(product5);
+console.log(product5); // Monitor de 27"
+
+// Mètodos de los Strings o cadenas de texto
+console.log(product.length); // 22
+console.log(product2.indexOf('30')); // 11
+console.log(product3.includes('pulgadas')); // true
