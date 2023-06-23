a8e3786c0daaca310aaa5fca8a90f06f417aba8d
DESKTOP-1TC6Q9U\USUARIO <aarroyaveduque20@gmail.com>
2023-06-22 19:51:24 -0500

    Manejo de boolens

---
+++ js/09-booleans.js
@@ -0,0 +1,12 @@
+// Booleans
+
+const boolean1 = true
+const boolean2 = false
+const boolean3 = "true"
+
+console.log(boolean1); // true - boolean
+console.log(boolean2); // false - boolean
+console.log(boolean3); // true - string
+
+const boolean4 = new Boolean(true)
+console.log(boolean4); // Boolean{true} - Object
