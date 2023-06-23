b1e003ac3478827f3312a741097efc769689575b
DESKTOP-1TC6Q9U\USUARIO <aarroyaveduque20@gmail.com>
2023-06-22 19:36:10 -0500

    Manejo de orden en las operaciones, incrementos y decrementos

---
+++ js/07-orden-operaciones.js
@@ -0,0 +1,33 @@
+// Orden de las operaciones
+
+let result
+
+result = 20 + 30 * 2
+console.log(result); // 80
+
+result = (20 + 30) * 2
+console.log(result); // 100
+
+result = (100 + 200 + 300) * .2
+console.log(result); // 120
+
+// Incrementos
+
+let score = 10
+
+score++
+console.log(score); // 11
+
+++score
+console.log(score); // 12
+
+score += 10
+console.log(score); // 20
+
+// Decremento
+
+score--
+console.log(score); // 11
+
+--score
+console.log(score); // 10
